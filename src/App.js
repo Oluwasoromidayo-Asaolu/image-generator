34326feb4e6f213e8045486489004250ed96b41f
XayZay <asaoluoluwasoromidayo@gmail.com>
2024-01-04 04:42:51 +0000

    Done with project

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
 import './App.css';
+import { useState } from 'react';
+import Loader from 'react-loader';
 import Logo from './components/Logo/Logo';
-// import Navigation from './components/Navigation/Navigation';
 import TextInput from './components/TextInput/TextInput';
 import ParticlesDesign from './components/ParticlesDesign/ParticlesDesign';
-import { useState } from 'react';
+
 
 function App() {
-const [text, setText] = useState('');
 const [image, setImage] = useState('');
+const [loading, setLoading] = useState(false);
+const [isButtonClicked, setisButtonClicked] = useState(false);
+const [apiSuccess, setApiSuccess] = useState(false);
+const [error, setError] = useState(false);
+
 const onButtonGenerate = () => {
+    setLoading(true);
+    setisButtonClicked(true);
+    setImage('');
     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
       .then(response => response.json())
       .then(result => {
-        console.log(result);
-        const imageBase64 = result.outputs[0].data.image.base64;
-        setImage(`data:image/jpeg;base64,${imageBase64}`);
+        if(result && result.outputs && result.outputs[0] && result.outputs[0].data && result.outputs[0].data.image && result.outputs[0].data.image.base64){
+          const imageBase64 = result.outputs[0].data.image.base64;
+          setImage(`data:image/jpeg;base64,${imageBase64}`);
+          setApiSuccess(true);
+        }
+        else{
+          setApiSuccess(false);
+        }
+        setLoading(false);
       })      
-    .catch(error => console.log('error', error));
+    .catch(error => {
+      setLoading(false);
+      setError(true);
+      console.log('error', error);
+    });
 }
+
 const downloadImage = () => {
   const downloadLink = document.createElement('a');
   downloadLink.href = image;
   downloadLink.download = 'generated-image.jpg';
-  // Trigger a click event on the link to prompt the download
   downloadLink.click();
 }
 const handleInputChange = (event) => {
   const updatedText = event.target.value;
   setRAW_TEXT(updatedText);
 }
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// // In this section, we set the user authentication, user and app ID, model details, and the prompt text we want
-// // to provide as an input. Change these strings to run your own example.
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// // Your PAT (Personal Access Token) can be found in the portal under Authentification
 const PAT = 'f08372c895234385b6aa5f4ccd5e1a5b';    
-// Specify the correct user_id/app_id pairings
-// Since you're making inferences outside your app's scope
 const USER_ID = 'openai';
 const APP_ID = 'dall-e';
-// Change these to whatever model and text you want to use
 const MODEL_ID = 'dall-e-3';
 const MODEL_VERSION_ID = 'dc9dcb6ee67543cebc0b9a025861b868';
 let [RAW_TEXT, setRAW_TEXT] = useState('');
-// To use a hosted text file, assign the URL variable
-// const TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
-
-///////////////////////////////////////////////////////////////////////////////////
-// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-///////////////////////////////////////////////////////////////////////////////////
 
 const raw = JSON.stringify({  
     "user_app_id": {
@@ -61,7 +65,6 @@ const raw = JSON.stringify({
         "data": {
           "text": {
                 "raw": RAW_TEXT
-              // "url": TEXT_FILE_URL
             }
         }
     }
@@ -82,24 +85,23 @@ const requestOptions = {
 
   return (
     <div>
-      {/* <Navigation />  */}
       <ParticlesDesign />
       <Logo />
       <div>
-        <p>Photon would generate an image based on your text. Give it a try!</p>
+        <p className='title'>Photon would generate an image based on your text. Give it a try!</p>
       </div>
       <TextInput handleInputChange={ handleInputChange } onButtonGenerate={ onButtonGenerate } />
-        <div id='generatedImage'>
-        {image && 
-          <img src={ image } alt='generatedImage' />
-        }
+      <div id='generatedImage'>
+        {isButtonClicked && !image && !loading && !apiSuccess && !error && <span>What??? Try something else please<span style={{ fontSize: '30px' }}>&#128578;</span></span>}
+        {image && !loading && <img src={image} alt='generatedImage' />}
+      </div>
+      <div className='loaderDiv'>
+        <div>{loading && !error && <span>Loading image</span>}</div>
+        <div>{loading && !error && <Loader color='#121212' className='loader' loaded={false}></Loader>}</div>
       </div>
       <div id='buttonDiv'>
-        {image && 
-          <button className='downloadBtn' onClick={downloadImage}>Download</button>
-        }
+        {image && !loading && <button className='downloadBtn' onClick={downloadImage}>Download</button>}
       </div>
-        
     </div>
   );
 }
--- src/components/ParticlesDesign/ParticlesDesign.js
+++ src/components/ParticlesDesign/ParticlesDesign.js
@@ -1,15 +1,11 @@
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
-//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+import './ParticlesDesign.css';
+import { loadSlim } from "tsparticles-slim"; 
 
 const ParticlesDesign = () => {
     const particlesInit = useCallback(async engine => {
         console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
         await loadSlim(engine);
     }, []);
 
@@ -19,6 +15,7 @@ const ParticlesDesign = () => {
 
     return (
         <Particles
+            className="particles"
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
